d6d09a6b57c6fe40dd66d2fb1cbe73debbe1539a
Ben Shillington <bdshilli@gmail.com>
2023-12-05 10:29:31 -0500

    ProjectController Edits

---
+++ demo/src/main/java/com/example/ProjectController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ProjectController {
+
+    @FXML
+    private button logoutButton;
+
+    @FXML
+    private button addColumnButton;
+
+    @FXML
+    private button editProjectTitleButton;
+
+    @FXML
+    private button editProjectDescriptionButton;
+
+    @FXML
+    private button settingsButton;
+}
