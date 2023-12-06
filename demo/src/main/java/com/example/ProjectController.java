11f07f381026a90ec203a3d07cb0675e94df5e14
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-12-06 14:26:48 -0500

    colin part

--- demo/src/main/java/com/example/ProjectController.java
+++ demo/src/main/java/com/example/ProjectController.java
@@ -1,8 +1,17 @@
 package com.example;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.*;
 
-public class ProjectController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import model.*;
+
+public class ProjectController implements Initializable{
 
     @FXML
     private Button add_collab_button;
@@ -13,6 +22,42 @@ public class ProjectController {
     @FXML
     private Button new_column_button;
 
+    @FXML
+    private HBox hbox_cols;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("In Project Controller");
+        UiFacade facade = UiFacade.getInstance();
+        facade.getProjectByName("Electric Missiles");
+        Project project = facade.getCurrentProject();
+        System.out.println("current project " + project);
+        ArrayList<Column> columns = project.getColumns();
+
+        for(Column column : columns){
+            System.out.println("Column " + column);
+            VBox columnBox = new VBox();
+            //columnBox.setW
+            columnBox.getStyleClass().add("column");
+            hbox_cols.getChildren().add(columnBox);
+            hbox_cols.setSpacing(10);
+            Label title = new Label();
+            title.getStyleClass().add("column_title");
+            title.setText(column.getColumnName());
+            columnBox.getChildren().add(title);
+
+            ArrayList<Task> tasks = column.getColumnTaskList();
+
+            for(Task task : tasks){
+                Label taskLabel = new Label();
+                taskLabel.setWrapText(true);
+                taskLabel.setText(task.getTaskName());
+                columnBox.getChildren().add(taskLabel);
+            }
+        }
+        
+     
+    }
 }
 
 
