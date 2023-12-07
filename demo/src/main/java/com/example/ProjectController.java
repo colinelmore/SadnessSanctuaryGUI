8ebee67aea6a4a6d569b194e3cbdbb04816daf48
bdshilli <bdshilli@gmail.com>
2023-12-06 23:53:47 -0500

    Add Column Completed

--- demo/src/main/java/com/example/ProjectController.java
+++ demo/src/main/java/com/example/ProjectController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
@@ -36,56 +38,82 @@ public class ProjectController implements Initializable {
         facade.getProjectByName("Electric Missiles");
         Project project = facade.getCurrentProject();
         System.out.println("current project " + project);
+        new_column_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                project.addColumn("New Column");
+                initializeColumn(project.getColumnByName("New Column"), facade);
+            }
+        });
+
         ArrayList<Column> columns = project.getColumns();
 
         for (Column column : columns) {
-            System.out.println("Column " + column);
-            VBox columnBox = new VBox();
-            // columnBox.setW
-            columnBox.getStyleClass().add("column");
-            hbox_cols.getChildren().add(columnBox);
-            hbox_cols.setSpacing(10);
-            Label title = new Label();
-            title.getStyleClass().add("column_title");
-            title.setText(column.getColumnName());
-            columnBox.getChildren().add(title);
-
-            ArrayList<Task> tasks = column.getColumnTaskList();
-
-            for (Task task : tasks) {
-                initializeTask(task, columnBox, column);
-            }
-
-            columnBox.setOnDragOver(new EventHandler<DragEvent>() {
-                public void handle(DragEvent event) {
-                    System.out.println("onDragOver");
-                    if (event.getGestureSource() != columnBox &&
-                            event.getDragboard().hasString()) {
-                        event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                    }
-                    event.consume();
-                }
-            });
-
-            columnBox.setOnDragDropped(new EventHandler<DragEvent>() {
-                public void handle(DragEvent event) {
-                    System.out.println("onDragDropped");
-                    Dragboard db = event.getDragboard();
-                    boolean success = false;
-                    if (db.hasString()) {
-                        Task copiedTask = facade.getTaskByName(db.getString());
-                        column.addColumnTask(copiedTask);
-                        initializeTask(copiedTask,columnBox,column);
-                        success = true;
-                    }
-                    event.setDropCompleted(success);
-
-                    event.consume();
-                }
-            });
-
+            initializeColumn(column, facade);
         }
 
+    }
+
+    private void initializeColumn(Column column, UiFacade facade) {
+        System.out.println("Column " + column);
+        VBox taskBox = new VBox();
+        // columnBox.setW
+        taskBox.getStyleClass().add("column");
+        hbox_cols.getChildren().add(taskBox);
+        hbox_cols.setSpacing(10);
+        Label title = new Label();
+        title.getStyleClass().add("column_title");
+        title.setText(column.getColumnName());
+        taskBox.getChildren().add(title);
+
+        Button addTask = new Button();
+        addTask.setText("Add Task");
+        addTask.setWrapText(true);
+        addTask.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ArrayList<Comment> comments = new ArrayList<>();
+                Task newTask = new Task("New Task", 1, new Date(), "Test notes", comments);
+                column.addColumnTask(newTask);
+                initializeTask(newTask, taskBox, column);
+            }
+        });
+        taskBox.getChildren().add(addTask);
+
+        ArrayList<Task> tasks = column.getColumnTaskList();
+
+        for (Task task : tasks) {
+            initializeTask(task, taskBox, column);
+        }
+
+        taskBox.setOnDragOver(new EventHandler<DragEvent>() { // Drag and Drop code sourced from
+                                                              // https://docs.oracle.com/javafx/2/drag_drop/jfxpub-drag_drop.htm
+            public void handle(DragEvent event) {
+                System.out.println("onDragOver");
+                if (event.getGestureSource() != taskBox &&
+                        event.getDragboard().hasString()) {
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+            }
+        });
+
+        taskBox.setOnDragDropped(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+                System.out.println("onDragDropped");
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    Task copiedTask = facade.getTaskByName(db.getString());
+                    column.addColumnTask(copiedTask);
+                    initializeTask(copiedTask, taskBox, column);
+                    success = true;
+                }
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
     }
 
     private void initializeTask(Task task, VBox columnBox, Column column) {
@@ -93,8 +121,7 @@ public class ProjectController implements Initializable {
         taskButton.setText(task.getTaskName());
         taskButton.setWrapText(true);
 
-        taskButton.setOnDragDetected(new EventHandler<MouseEvent>() { // Drag and Drop code sourced from
-                                                                      // https://docs.oracle.com/javafx/2/drag_drop/jfxpub-drag_drop.htm
+        taskButton.setOnDragDetected(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 Dragboard db = taskButton.startDragAndDrop(TransferMode.ANY);
                 ClipboardContent taskContent = new ClipboardContent();
